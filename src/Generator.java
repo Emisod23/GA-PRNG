4e4e2141d4d2b5799a49036494ed3a50dd2710de
Hera-heeto <60388888+Emisod23@users.noreply.github.com>
2022-05-23 17:23:21 +0200

    start of seed generation but not finished

--- src/Generator.java
+++ src/Generator.java
@@ -1,5 +1,11 @@
-public class Generator {
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class Generator {
+    public static Random GetSeed(){
+        Random seed = new Random(System.currentTimeMillis());
+        return seed;
+    }
+    public static void main(String[] args) {
+        System.out.println(GetSeed());
     }
 }
